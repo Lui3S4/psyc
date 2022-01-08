97457b414c33e013bd0544df87bbaa281bf31f61
Lui3S4 <96251993+Lui3S4@users.noreply.github.com>
2022-01-08 12:51:31 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -64,7 +64,10 @@ class CreditsState extends MusicBeatState
 			['ninjamuffin99',		'ninjamuffin99',	"Programmer of Friday Night Funkin'",				'https://twitter.com/ninja_muffin99',	'0xFFF73838'],
 			['PhantomArcade',		'phantomarcade',	"Animator of Friday Night Funkin'",					'https://twitter.com/PhantomArcade3K',	'0xFFFFBB1B'],
 			['evilsk8r',			'evilsk8r',			"Artist of Friday Night Funkin'",					'https://twitter.com/evilsk8r',			'0xFF53E52C'],
-			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",					'https://twitter.com/kawaisprite',		'0xFF6475F3']
+			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",					'https://twitter.com/kawaisprite',		'0xFF6475F3'],
+                        [''],
+                        ["Creator' CookieEngine"],
+			['DotorBiscoito',		'Biscoito',	"Programmer of Cookie Engine'",				'https://youtube.com/channel/UCq6YSL7iO6mkOFI7fLe_GPA',	'0xFFF86819']
 		];
 		
 		for(i in pisspoop){
